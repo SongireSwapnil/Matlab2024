372fe297bea78a3c49f8d8eb4c2506a42c342d27
SongireSwapnil <108467500+SongireSwapnil@users.noreply.github.com>
2024-08-27 21:56:45 +0530

    updated 25

--- Driving_Mode_Status.m
+++ Driving_Mode_Status.m
@@ -1,6 +1,7 @@
 classdef Driving_Mode_Status < Simulink.IntEnumType
     enumeration
         Drive_Mode_Active(1)
-        Reverse_Mode_Active(0)
+        Reverse_Mode_Active(2)
+        park_mode(3)
     end    
 end
